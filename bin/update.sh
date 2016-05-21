fdce613e783a82f4e1f5af3375eb19976963a2de
Georgi Krastev <joro.kr.21@gmail.com>
2016-05-21 10:47:18 +0200

    Added ruby gems and npm packages to update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt-get autoclean
 sudo apt-get clean
-sudo apt-get autoremove
-conda update --all
+sudo apt-get autoremove -y
+conda update --all -y
 rbenv update
+sudo gem update --system
+sudo gem update
+npm update -g
 youtube-dl -U
